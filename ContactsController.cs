fac236949a937e272c74a3a25b2ec12cd86f74c9
Mike <zarate302_5@hotmail.com>
2017-05-18 19:28:04 -0500

    getting started Electron Angular Net Core

---
+++ ContactsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        // GET api/contacts
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new [] {
+                new { FirstName = "John", LastName = "Doe" },
+                new { FirstName = "Mike", LastName = "Smith" }
+            };
+
+            return Ok(result);
+        }
+    }
+}
